95d684ecc60fa43d054d59cd7744fa4e645a0077
un5t0ppab13 <devbrights@gmail.com>
2015-09-22 13:53:11 -0700

    rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :users
   resources :pins
   root "pins#index"
   # The priority is based upon order of creation: first created -> highest priority.
