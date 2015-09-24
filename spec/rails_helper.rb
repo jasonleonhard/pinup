9845df0496829d3ed040ab5dffe0da57421ed82e
un5t0ppab13 <devbrights@gmail.com>
2015-09-24 12:28:45 -0700

    rails_helper.rb added  Capybara.javascript_driver = :webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,8 @@ require 'simple_bdd'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  Capybara.javascript_driver = :webkit
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
