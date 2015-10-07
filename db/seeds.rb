eec44763c68835fdb1c800c9dbf3b2b316955d4b
un5t0ppab13 <devbrights@gmail.com>
2015-10-06 20:19:14 -0700

    deleted heroku dyno

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ require 'ffaker'
     :title => FFaker::Name.name,
     :description => FFaker::HipsterIpsum.sentence(11),
     :image => File.new(Dir["app/assets/images/random/*.jpg"].sample)
-    # :image => File.new(Dir["public/assets/random/*.jpg"].sample)
   )
   puts pin.inspect
 end
