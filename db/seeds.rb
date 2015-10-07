5d0809a1ee3ccd85da562a5dc4ec71fb6b650e89
un5t0ppab13 <devbrights@gmail.com>
2015-10-06 20:09:26 -0700

    gphm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ require 'ffaker'
     :user_id => 1,
     :title => FFaker::Name.name,
     :description => FFaker::HipsterIpsum.sentence(11),
-    # :image => File.new(Dir["app/assets/images/random/*.jpg"].sample)
-    :image => File.new(Dir["public/assets/random/*.jpg"].sample)
+    :image => File.new(Dir["app/assets/images/random/*.jpg"].sample)
+    # :image => File.new(Dir["public/assets/random/*.jpg"].sample)
   )
   puts pin.inspect
 end
