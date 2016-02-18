2ba412e59f4fd9f66953e786d95415b335fc9b24
un5t0ppab13 <devbrights@gmail.com>
2016-02-18 11:27:02 -0800

    refactor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,10 @@ Rails.application.configure do
           # push sqlite development db to:
                            # pg production db! 
           # https://www.youtube.com/watch?v=p_3dIPgXgkg
-  ########################################################################################################
-  
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  ########################################################################################################
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
